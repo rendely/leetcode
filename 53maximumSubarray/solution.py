358e8544839a9b746ff2d507fc2cbb35c6e49788
matthew rendely <matthew.rendely@gmail.com>
2023-12-07 23:31:28 -0800

    Fixed optimized solution

--- 53maximumSubarray/solution.py
+++ 53maximumSubarray/solution.py
@@ -3,12 +3,12 @@
 class Solution:
     def maxSubArray(self, nums):
         max_so_far = None
-        for start in range(0,len(nums)):
+        for start in range(0,len(nums)):  
             sum_so_far = 0
-            for end in range(start+1, len(nums)+1):
-                this_sum = sum_so_far + nums[end-1]
-                if  max_so_far is None or this_sum > max_so_far:
-                    max_so_far = this_sum
+            for end in range(start, len(nums)):
+                sum_so_far = sum_so_far + nums[end]
+                if  max_so_far is None or sum_so_far > max_so_far:
+                    max_so_far = sum_so_far
         return max_so_far
 
     def maxSubArrayNaive(self, nums):
