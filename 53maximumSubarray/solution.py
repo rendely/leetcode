b8bb620a3584de92ce78238dfc21fa6208fa0b8e
matthew rendely <matthew.rendely@gmail.com>
2023-12-07 22:37:42 -0800

    Naive O(n^2) solution works on short arrays but too inefficient for long arrays

--- 53maximumSubarray/solution.py
+++ 53maximumSubarray/solution.py
@@ -2,8 +2,21 @@
 
 class Solution:
     def maxSubArray(self, nums):
-        #
-        return sum(nums)
+        max_so_far = None
+        for start in range(0,len(nums)):
+            for end in range(start+1, len(nums)+1):
+                this_sum = sum(nums[start:end])
+                if  max_so_far is None or this_sum > max_so_far:
+                    max_so_far = this_sum
+        return max_so_far
+    def maxSubArrayNaive(self, nums):
+        max_so_far = None
+        for start in range(0,len(nums)):
+            for end in range(start+1, len(nums)+1):
+                this_sum = sum(nums[start:end])
+                if  max_so_far is None or this_sum > max_so_far:
+                    max_so_far = this_sum
+        return max_so_far
 
 
 if __name__ == '__main__':
