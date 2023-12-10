64281ea85f5704f15247ba8de793b79d054037c6
matthew rendely <matthew.rendely@gmail.com>
2023-12-09 19:03:30 -0800

    Impl brute force recursive solution

--- 198houseRobber/solution.py
+++ 198houseRobber/solution.py
@@ -2,8 +2,12 @@
 
 
 class Solution:
-    def rob(self, nums):
-        pass
+    def robRecursiveBrute(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        return max(nums[0] + self.rob(nums[2:]), self.rob(nums[1:]))              
 
 
 '''
@@ -21,4 +25,12 @@ E.g.
 between 2 and 7 you can't choose seven, because then in next pair your max total is 10
 whereas if you pick 2 your max total is 11
 Should be solvable in O(n)
+
+[2,50,1,50,100]
+In this new example it's not enough to look at first four nums
+if you did you'd pick 50 and 50
+[1,1,90,100,20]
+In this example, also not good algo to start with largest number, you'd miss 90 + 20
+You would never skip more than two in a row because you'd be giving up a free num
+
 '''
