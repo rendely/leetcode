28c0892d784f5dce6bc66894fd8b3b287dc0ac71
matthew rendely <matthew.rendely@gmail.com>
2023-12-09 18:20:58 -0800

    Write up notes on solution

--- 198houseRobber/solution.py
+++ 198houseRobber/solution.py
@@ -4,3 +4,21 @@
 class Solution:
     def rob(self, nums):
         pass
+
+
+'''
+Notes
+Lets try to break down the example given into smaller sub problems
+[2,7,9,3,1]
+For any pair of numbers pick the greater one
+2,7 -> 7
+7,9 -> 9
+9,3 -> 9
+3,1 -> 3
+As you move from left to right, pick the greater of the pair if it will not 
+prevent you from getting an even bigger maximum by choosing adjacent in the next pair
+E.g.
+between 2 and 7 you can't choose seven, because then in next pair your max total is 10
+whereas if you pick 2 your max total is 11
+Should be solvable in O(n)
+'''
