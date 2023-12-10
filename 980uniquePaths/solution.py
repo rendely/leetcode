e6c9e0817b9fb3e5f56584060a5f1e1d0163ad47
matthew rendely <matthew.rendely@gmail.com>
2023-12-10 10:27:12 -0800

    Implement method to locate start and end

--- 980uniquePaths/solution.py
+++ 980uniquePaths/solution.py
@@ -4,6 +4,16 @@
 class Solution:
    def uniquePathsIII(self, grid):
         pass
+   def getStartEnd(self, grid):
+        num_rows = len(grid)
+        num_cols = len(grid[0])
+        for i in range(0, num_rows):
+            for j in range(0, num_cols):
+                if grid[i][j] == 1:
+                    start = (i,j)
+                if grid[i][j] == 2:
+                    end = (i,j)
+        return {"start": start, "end": end}
         
 '''
 Notes
@@ -13,5 +23,8 @@ Notes
 if __name__ == '__main__':
     #debugging
     solution = Solution()
+    grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+    startEnd = solution.getStartEnd(grid)
+    print(startEnd)
     ans = solution.uniquePathsIII([[]])
     print(f'{ans=}')
