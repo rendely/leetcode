ea77d902c9afa81218cf7b1b38d2bc50842ea837
matthew rendely <matthew.rendely@gmail.com>
2024-03-25 12:43:05 -0700

    Outline solution

--- 24swapNodesInParis/solution.py
+++ 24swapNodesInParis/solution.py
@@ -23,3 +23,11 @@ input = ListNode(1,ListNode(2,ListNode(3,ListNode(4))))
 print(input)
 output = Solution().swapPairs(input)
 print(output)
+
+# If empty return same empty 
+# If one value, return as is
+# If two nodes, swap them
+# If three nodes, swap first two
+# We need to detect to stop when there are either 0 or only 0 nodes after the current two working set
+# To swap nodes A and B, we need to set B.next to A, and A.next to B.next
+# Additionally, we need to ensure whatever node was before A now points to B
