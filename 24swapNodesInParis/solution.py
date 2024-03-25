7335dd7f53c7b6284873af2542edec44d08dc9e2
matthew rendely <matthew.rendely@gmail.com>
2024-03-25 12:40:14 -0700

    Add repr method for testing and debugging

--- 24swapNodesInParis/solution.py
+++ 24swapNodesInParis/solution.py
@@ -7,6 +7,19 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+    def __repr__(self):
+        vals = [self.val]
+        next = self.next
+        while next:
+            vals.append(next.val)
+            next = next.next
+        return f'{vals}'
+    
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        return head
+    
+input = ListNode(1,ListNode(2,ListNode(3,ListNode(4))))
+print(input)
+output = Solution().swapPairs(input)
+print(output)
--- 24swapNodesInParis/test_solution.py
+++ 24swapNodesInParis/test_solution.py
@@ -1,12 +1,12 @@
 import pytest
-from solution import Solution
+from solution import Solution, ListNode
 
 
 solution = Solution();
 
 class TestSolution:
     def test_one(self):  
-        input = [1,2,3,4]     
+        input = ListNode(1,ListNode(2,ListNode(3,ListNode(4))))
         output = solution.swapPairs(input)
-        expected_output = [2,1,4,3]
+        expected_output = ListNode(2,ListNode(1,ListNode(4,ListNode(3))))
         assert output == expected_output
