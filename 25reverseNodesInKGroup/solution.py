66f379f824830b1739e736d7a8246d4684457936
matthew rendely <matthew.rendely@gmail.com>
2024-03-31 11:33:32 -0700

    Write out ideas for how to solve, some starter code

--- 25reverseNodesInKGroup/solution.py
+++ 25reverseNodesInKGroup/solution.py
@@ -17,4 +17,61 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        pass
+        if k == 1 or head.next is None:
+            return head
+        
+        new_head = None
+        new_tail = None
+        count = 0
+        is_next = head.next
+
+        # keep going until all nodes used
+        while is_next is not None:
+        
+        # if we run out of nodes before count == k, append rest as-
+
+
+        
+
+        return new_head
+
+'''
+Solution brainstorm #1
+
+If k = 1, just return the head
+If n <= 1, just return the head
+
+Starting from the initial head, break it off into another list with a reference to the new head (next) and a cursor pointing at the end of this new list
+
+Break of the new head and prepend to the new list, repeating this until you either reach K nodes in the new list OR there is no next node in the original list
+
+If there is a next node, add it after the cursor, and then start prepending before the cursor as before.
+
+If there is not a next node, we need to undo this batch of reversals
+
+Example with even number k = 2
+[1,2,3,4,5,6]
+[1] [2,3,4,5,6]
+[2,1] [3,4,5,6]
+[2,1] [3] [4,5,6]
+[2,1] [4,3] [5,6]
+[2,1] [4,3] [5] [6]
+[2,1] [4,3] [6,5]
+
+Example with remainder k = 4
+[1,2,3,4,5,6,7]
+[1] [2,3,4,5,6,7]
+[2,1] [3,4,5,6,7]
+[3,2,1] [4,5,6,7]
+[4,3,2,1] [5,6,7]
+[4,3,2,1] [5] [6,7]
+[4,3,2,1] [6,5] [7]
+[4,3,2,1] [7,6,5] oops not enough
+
+
+Solution brainstorm #2 maybe a cleaner recursive solution?
+[1,2,3,4,5,6] with k = 2
+reverse([1,2]) + reverse([3,4,5,6])
+reverse([1,2]) + ( reverse([3,4]) + reverse([5,6])
+
+''' 
--- 25reverseNodesInKGroup/test_solution.py
+++ 25reverseNodesInKGroup/test_solution.py
@@ -6,12 +6,12 @@ solution = Solution();
 class TestSolution:
     def test_4_K_2(self):  
         input = ListNode(1,ListNode(2,ListNode(3,ListNode(4))))
-        output = solution.swapPairs(input)
+        output = solution.reverseKGroup(input, 2)
         expected_output = ListNode(2,ListNode(1,ListNode(4,ListNode(3))))
         assert f'{output}' == f'{expected_output}'
 
     def test_4_K_3(self):  
         input = ListNode(1,ListNode(2,ListNode(3,ListNode(4))))
-        output = solution.swapPairs(input)
+        output = solution.reverseKGroup(input, 3)
         expected_output = ListNode(3,ListNode(2,ListNode(1,ListNode(4))))
         assert f'{output}' == f'{expected_output}'
