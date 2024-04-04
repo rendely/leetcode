c8d35a68db90db8bb2a7295b1b6ea11285739522
matthew rendely <matthew.rendely@gmail.com>
2024-04-03 21:52:29 -0700

    Add test for dupe words

--- 30substringConcatOfWords/test_solution.py
+++ 30substringConcatOfWords/test_solution.py
@@ -4,16 +4,23 @@ from solution import Solution
 solution = Solution();
 
 class TestSolution:
-    def test_one(self):  
+    def test_two(self):  
         s = "barfoothefoobarman"
         words = ["foo","bar"]
         output = solution.findSubstring(s, words)
         expected_output = [0,9]
         assert output == expected_output   
 
-    def test_two(self):  
+    def test_none(self):  
         s = "wordgoodgoodgoodbestword"
         words = ["word","good","best","word"]
         output = solution.findSubstring(s, words)
         expected_output = []
         assert output == expected_output     
+
+    def test_dupe_words(self):  
+        s = "wordgoodgoodgoodbestword"
+        words = ["word","good","best","good"]
+        output = solution.findSubstring(s, words)
+        expected_output = [8]
+        assert output == expected_output
