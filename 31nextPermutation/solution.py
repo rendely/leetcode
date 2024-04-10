058ae4e9ac2a0ef1baf4be983d8cd29e2287e928
matthew rendely <matthew.rendely@gmail.com>
2024-04-09 19:03:27 -0700

    Add sort function which uses bubble sort to swap

--- 31nextPermutation/solution.py
+++ 31nextPermutation/solution.py
@@ -7,6 +7,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+    def sort(self, nums: List[int], start_index=0) -> None:
+        for i in range(start_index, len(nums)):
+            for j in range(i+1,len(nums)):
+                # print(i,j, nums) 
+                left = nums[i]
+                right = nums[j]
+                if left > right:
+                    nums[i] = right
+                    nums[j] = left
+                       
+nums = [3,1,4,5,1,3,0]
+Solution().sort(nums)
+print(nums)
         
 
 """
@@ -31,5 +44,27 @@ Testing on example:
 [1,4,3,2] => [2,1,3,4] RIGHT
 
 [1,2,3,4] => [1,2,4,3] RIGHT
+
+
+Example going up
+1234
+1243 swap last and second last
+1324 swap last and second last and then middle
+1342
+1423
+1432
+2134
+
+"""
+
+"""
+Swap sorting algorithm
+
+3121
+1321
+1231
+1213
+
+
 
 """
