f49f9233443fbfefabeac436367b1f17e17e52f1
matthew rendely <matthew.rendely@gmail.com>
2024-04-11 23:09:03 -0700

    Add problem 32

---
+++ 32longestValidParentheses/solution.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/longest-valid-parentheses/description/
+
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        return 0
---
+++ 32longestValidParentheses/test_solution.py
@@ -0,0 +1,23 @@
+import pytest
+from solution import Solution
+
+solution = Solution();
+
+class TestSolution:
+    def test_zero(self):  
+        s = ""
+        output = solution.longestValidParentheses(s)
+        expected_output = 0
+        assert output == expected_output
+
+    def test_two(self):  
+        s = "(()"
+        output = solution.longestValidParentheses(s)
+        expected_output = 2
+        assert output == expected_output
+
+    def test_four(self):  
+        s = ")()())"
+        output = solution.longestValidParentheses(s)
+        expected_output = 4
+        assert output == expected_output
