453c54c300c974dc617ad59199885cd2f96ef4b5
matthew rendely <matthew.rendely@gmail.com>
2024-04-15 21:25:26 -0700

    Add solution notes for 32

--- 32longestValidParentheses/solution.py
+++ 32longestValidParentheses/solution.py
@@ -4,3 +4,18 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         return 0
+    
+'''
+solution notes
+( = open
+) = close
+
+first character must be open
+last character must be close
+total # opens = # closes
+at any given point you can always add an open
+at any given point you can only add a close if # opens > # closes
+at any given point you can only add an open if # opens - # closes < remaining
+characters in the string
+
+'''
